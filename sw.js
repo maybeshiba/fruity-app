f1ce67630a237e9298a18e5203acc85fd6ea25a7
Shibi <maybeshiba@gmail.com>
2026-03-04 02:16:56 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'fruity-v1';
+const CACHE = 'fruity-v2';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:wght@400;600;700;800;900&display=swap'
+  '/fruity-app/',
+  '/fruity-app/index.html',
+  '/fruity-app/manifest.json',
+  '/fruity-app/icon-192.png',
+  '/fruity-app/icon-512.png',
 ];
 
 self.addEventListener('install', e => {
@@ -21,6 +21,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
